c7c2fee31dc895cb1e638595fcd72c2d4355c5a3
Teja-2024 <166777391+Teja-2024@users.noreply.github.com>
2024-07-04 16:37:35 +0530

    Update example_addr.py

--- example_addr.py
+++ example_addr.py
@@ -3,6 +3,5 @@ address.setStreet(street);
 address.setCountry(country);
 address.setStateOrProvince(state);
 address.setCity(city);
-
 address.setHouseNumberOrName(number);
 address.setPostalCode(postalCode);
