c221293fbea0ba63232d749b9c5642a9acab09ac
Teja-2024 <166777391+Teja-2024@users.noreply.github.com>
2024-07-05 00:33:19 +0530

    Delete example_addr.py

--- example_addr.py
+++
@@ -1,7 +0,0 @@
-address = Address();
-address.setStreet(street);
-address.setCountry(country);
-address.setStateOrProvince(state);
-address.setCity(city);
-address.setHouseNumberOrName(number);
-address.setPostalCode(postalCode);
