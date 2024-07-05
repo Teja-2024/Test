582dca0eeef6bbad0b77b43a44c51efdd69c3535
Teja-2024 <166777391+Teja-2024@users.noreply.github.com>
2024-07-05 11:39:02 +0530

    Update example_addr.py

--- example_addr.py
+++ example_addr.py
@@ -1,18 +1,8 @@
- "home_phone": "str", 
+
 address = Address();
 
 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
-lookup = {'UserName': usernameUID, 'Browser Information':browserInfoUID, 'TimeStamp':timestampUID, 'url':urlUID} 
 
 
- "home_phone": "str", 
+
+
